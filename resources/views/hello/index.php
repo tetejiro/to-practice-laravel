8d3920aa87a2f7c84f9e6d257e1f8fb066da5de6
tetejiro <hy1733505@gmail.com>
2023-01-08 02:42:26 +0900

    ルーティングでコントローラを呼び出し、コントローラにより呼び出されたviewを表示している。（getパラメータ使用バージョン）

---
+++ resources/views/hello/index.php
@@ -0,0 +1,14 @@
+<heml>
+  <head>
+    <title>Hello/Index</title>
+    <style>
+        body {font-size:16px; color:#999; }
+        h1 {font-size:120pt; text-align:right; color:#fafafa; margin:-50px 0px -120px 0px;}
+    </style>
+  </head>
+  <body>
+    <h1>Index</h1>
+    <p><?php echo $msg; ?></p>
+    <p>ID=<?php echo $id; ?></p>
+  </body>
+  </heml>
