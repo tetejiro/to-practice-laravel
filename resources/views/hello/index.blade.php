36b89acf8b91dfb32aea39f8e04f2cfe4294cd37
tetejiro <hy1733505@gmail.com>
2023-01-11 20:04:44 +0900

    バリデーションメソッド（基底クラスのメソッド）使用例

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -8,7 +8,48 @@
 @endsection
 
 @section('content')
-  <p>ここが本文のコンテンツです。</p>
-  <p>これは、<middleware>google.com</middleware>へのリンクです。</p>
-  <p>これは、<middleware>yahoo.co.jp</middleware>へのリンクです。</p>
+  <p>{{$msg}}</p>
+  <form method="POST" action="/hello">
+    @if(count($errors) > 0)
+      <p>入力に問題があります。再入力してください。</p>
+    @endif
+  <form action="/hello" method="post">
+  <table>
+    @csrf
+    @if ($errors->has('name'))
+      <tr>
+        <th>エラー：</th>
+        <td>{{$errors->first('name')}}</td>
+      </tr>
+    @endif
+    <tr>
+      <th>name:</th>
+      <td><input type="text" name="name" value="{{old('name')}}"></td>
+    </tr>
+    @if ($errors->has('mail'))
+      <tr>
+        <th>エラー：</th>
+        <td>{{$errors->first('mail')}}</td>
+      </tr>
+    @endif
+    <tr>
+      <th>mail:</th>
+      <td><input type="text" name="mail" value="{{old('mial')}}"></td>
+    </tr>
+    @if ($errors->has('age'))
+      <tr>
+        <th>エラー：</th>
+        <td>{{$errors->first('age')}}</td>
+      </tr>
+    @endif
+    <tr>
+      <th>age:</th>
+      <td><input type="text" name="age" value="{{old('age')}}"></td>
+    </tr>
+    <tr>
+      <th></th>
+      <td><input type="submit" value="send"></td>
+    </tr>
+  </table>
+  </form>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('hello', 'App\Http\Controllers\HelloController@index')->middleware(HelloMiddleware::class);
-// Route::post('hello', 'App\Http\Controllers\HelloController@post');
+Route::get('hello', 'App\Http\Controllers\HelloController@index');
+Route::post('hello', 'App\Http\Controllers\HelloController@post');
