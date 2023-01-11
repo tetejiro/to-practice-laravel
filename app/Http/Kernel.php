a7106728d545caaa352660dfdb0e8821af09896c
tetejiro <hy1733505@gmail.com>
2023-01-11 20:03:54 +0900

    グローバルMiddlewareの登録 グループグローバルMiddlewareの登録

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,6 +21,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        \App\Http\Middleware\HelloMiddleware::class,
     ];
 
     /**
@@ -42,6 +43,10 @@ class Kernel extends HttpKernel
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
+
+        'hell' => [
+            \App\Http\Middleware\HelloMiddleware::class,
         ],
     ];
 
