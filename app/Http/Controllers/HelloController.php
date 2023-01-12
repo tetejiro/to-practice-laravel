3aa9be1041c8ebb8da67f46b69dbb12005aa7574
tetejiro <hy1733505@gmail.com>
2023-01-13 02:43:21 +0900

    フォームリクエストを使用したvalidation使用例

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -4,19 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use App\Http\Requests\HelloRequest;
 
 class HelloController extends Controller {
     public function index(Request $request) {
         return view('hello.index', ['msg'=>'フォームを入力']);
     }
 
-    public function post(Request $request) {
-        $validate_rule = [
-            'name' => 'required',
-            'mail' => 'email',
-            'age' => 'numeric|between:0,150',
-        ];
-        $this->validate($request, $validate_rule);
+    public function post(HelloRequest $request) {
         return view('hello.index', ['msg'=>'正しく入力されました！']);
     }
 }
