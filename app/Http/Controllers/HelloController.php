f44f2e82e5bbcc4cabf1a5ab601f6f2ff667932c
tetejiro <hy1733505@gmail.com>
2023-01-13 22:40:27 +0900

    validate使用例③：validatorクラスのインスタンスを生成する方法

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -11,7 +11,15 @@ class HelloController extends Controller {
         return view('hello.index', ['msg'=>'フォームを入力']);
     }
 
-    public function post(HelloRequest $request) {
+    public function post(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'mail' => 'email',
+            'age' => 'numeric|between:0,150',
+        ]);
+        if($validator->fails()) {
+            return redirect('/hello')->withErrors($validator)->withInput();
+        }
         return view('hello.index', ['msg'=>'正しく入力されました！']);
     }
 }
