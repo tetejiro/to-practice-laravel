6653669337fc00f8a669114c103ee64ff2da0fec
tetejiro <hy1733505@gmail.com>
2023-01-20 01:30:09 +0900

    insert, update, delete, select 追記完了

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -31,6 +31,36 @@ class HelloController extends Controller {
             'age' => $request->age,
         ];
         DB::insert('insert into people (name, mail, age) values (:name, :mail, :age)', $param);
-        return redirect('hello');
+        return redirect('./hello');
+    }
+
+    public function edit(Request $request) {
+        $param = ['id' => $request->id];
+        $item = DB::select('select * from people where id = :id', $param);
+        return View('hello.edit', ['form' => $item[0]]);
+    }
+
+    public function update(Request $request) {
+        $param = [
+            'id' => $request->id,
+            'name' => $request->name,
+            'mail' => $request->mail,
+            'age' => $request->age,
+        ];
+        DB::update('update people set name = :name, mail = :mail, age = :age where id = :id', $param);
+        return redirect('./hello');
+    }
+
+    public function del(Request $request) {
+        $param = ['id' => $request->id];
+        $item = DB::select('select * from people where id = :id', $param);
+        return view('hello.del', ['form' => $item[0]]);
+    }
+
+    public function remove(Request $request) {
+        print 'remove';
+        $param = [ 'id' => $request->id];
+        DB::delete('delete from people where id = :id', $param);
+        return redirect('/hello');
     }
 }
