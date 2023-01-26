afee2fb0096f65d512dba0ce1d5d0704b33075eb
tetejiro <hy1733505@gmail.com>
2023-01-27 00:11:35 +0900

    eloquent:修正

--- routes/web.php
+++ routes/web.php
@@ -35,4 +35,4 @@ Route::get('hello/show', 'App\Http\Controllers\HelloController@show');
 Route::get('person', 'App\Http\Controllers\PersonController@index');
 
 Route::get('person/find', 'App\Http\Controllers\PersonController@find');
-Route::post('person/find', 'App\Http\Controllers\PersonController@find');
+Route::post('person/find', 'App\Http\Controllers\PersonController@search');
