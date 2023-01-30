2a883f91c32ebb12b319cb9b51ffaa48f3004390
tetejiro <hy1733505@gmail.com>
2023-01-30 22:50:47 +0900

    グローバルスコープ使用例

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -6,6 +6,11 @@ use App\Models\Person;
 use Illuminate\Http\Request;
 
 class PersonController extends Controller {
+    public function index(Request $request) {
+        $items = Person::all();
+        return view('person.index', ['items' => $items]);
+    }
+
     public function find(Request $request) {
         return view('person.find', ['input' => '']);
     }
