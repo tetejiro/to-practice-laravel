0c1d7a28ae0f72704ed3f81e7d0ed9f22145bef5
tetejiro <hy1733505@gmail.com>
2023-02-02 18:55:26 +0900

    Eloquentモデル：インサート使用例

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -9,26 +9,38 @@ use App\Scopes\ScopePerson;
 
 class Person extends Model
 {
-    protected static function boot() {
-        parent::boot();
+    // protected static function boot() {
+    //     parent::boot();
 
-        static::addGlobalScope(new ScopePerson);
-    }
-    use HasFactory;
+    //     static::addGlobalScope(new ScopePerson);
+    // }
+    // use HasFactory;
+
+    // public function getData() {
+    //     return $this->id. ': ' .$this->name.' ('.$this->age.')';
+    // }
+
+    // public function scopeNameEqual($query, $str) {
+    //     return $query->where('name', $str);
+    // }
+
+    // public function scopeAgeGreaterThan($query, $int) {
+    //     return $query->where('age', '>=', $int);
+    // }
+
+    // public function scopeAgeLessThan($query, $int) {
+    //     return $query->where('age', '<=', $int);
+    // }
+
+    protected $guarded = array('id');
+
+    public static $rules = array(
+        'name' => 'required',
+        'mail' => 'email',
+        'age' => 'integer|min:0|max:150'
+    );
 
     public function getData() {
-        return $this->id. ': ' .$this->name.' ('.$this->age.')';
-    }
-
-    public function scopeNameEqual($query, $str) {
-        return $query->where('name', $str);
-    }
-
-    public function scopeAgeGreaterThan($query, $int) {
-        return $query->where('age', '>=', $int);
-    }
-
-    public function scopeAgeLessThan($query, $int) {
-        return $query->where('age', '<=', $int);
+        return $this->id. ':' .$this->name.'('.$this->age.')';
     }
 }
