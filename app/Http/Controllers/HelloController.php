bf72eb4864ae692db749ad9c3e46272e40884601
tetejiro <hy1733505@gmail.com>
2023-02-12 16:38:48 +0900

    session利用①

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -69,4 +69,15 @@ class HelloController extends Controller {
     public function rest(Request $request){
         return view('rest.create');
     }
+
+    public function ses_get(Request $request) {
+        $sesdata = $request->session()->get('msg');
+        return view('hello.session', ['session_data' => $sesdata]);
+    }
+
+    public function ses_put(Request $request) {
+        $msg = $request->input;
+        $request->session()->put('msg', $msg);
+        return redirect('hello/session');
+    }
 }
